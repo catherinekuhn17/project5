e95326d0a0f8212b7d056442e2962d4dc7ff498d
ckuhn <catherinekuhn17@gmail.com>
2022-02-05 19:07:26 -0800

    adding kmeans alg

--- cluster/kmeans.py
+++ cluster/kmeans.py
@@ -19,6 +19,12 @@ class KMeans:
             max_iter: int
                 the maximum number of iterations before quitting model fit
         """
+        self.k = k
+        self.metric = metric
+        self.tol = tol
+        self.max_iter = max_iter
+        self._err = np.inf
+        
     
     def fit(self, mat: np.ndarray):
         """
@@ -29,6 +35,48 @@ class KMeans:
                 A 2D matrix where the rows are observations and columns are features
         """
 
+        ctrs_idx = np.random.choice(len(mat), self.k, replace=False)
+        centroids = mat[ctrs_idx, :]
+        dist_to_centroids = cdist(mat, centroids, self.metric)
+        closest_centroid = np.array([np.argsort(d)[0] for d in dist_to_centroids])
+
+        itr=0
+        while itr < max_iter: # max itterations to go for before exiting
+            centroids = []
+            for k_idx in range(self.k):
+                centroids.append(mat[closest_centroid==k_idx].mean(axis=0))
+            dist_to_centroids = cdist(mat, centroids)
+            closest_centroid = np.array([np.argsort(d)[0] for d in dist_to_centroids])
+            itr+=1
+            new_err = calc_mse(mat, self.k, closest_centroid) 
+            if self._err - new_err < tol:
+                self._err = new_err
+                break
+            self._err = new_err
+
+        self.centroids = centroids  
+
+    def _calc_mse(self, mat, closest_centroid):
+        """
+        calculates the mean square error of a matrix and the centroids
+        
+        inputs:
+            mat: np.ndarray
+                A 2D matrix where the rows are observations and columns are features             closest_centroid:
+                the closest centroid to each point in mat
+        returns:
+            float
+                the mean square error score
+        """
+        sq_err = []
+        for k_idx in range(self.k):
+            n = len(mat[closest_centroid==k_idx])
+            num = sum(np.square(cdist(mat[closest_centroid==k_idx], 
+                                    [centroids[k_idx]])))
+            sq_err.append((num/n)[0])
+
+        return sum(sq_err)
+    
     def predict(self, mat: np.ndarray) -> np.ndarray:
         """
         predicts the cluster labels for a provided 2D matrix
@@ -41,7 +89,10 @@ class KMeans:
             np.ndarray
                 a 1D array with the cluster label for each of the observations in `mat`
         """
-
+        dist_to_centroids = cdist(mat, self.centroids)
+        closest_centroid = np.array([np.argsort(d)[0] for d in dist_to_centroids])
+        return closest_centroid
+    
     def get_error(self) -> float:
         """
         returns the final squared-mean error of the fit model
@@ -50,6 +101,7 @@ class KMeans:
             float
                 the squared-mean error of the fit model
         """
+        return self._err
 
     def get_centroids(self) -> np.ndarray:
         """
@@ -59,3 +111,5 @@ class KMeans:
             np.ndarray
                 a `k x m` 2D matrix representing the cluster centroids of the fit model
         """
+        return self.centroids
+
